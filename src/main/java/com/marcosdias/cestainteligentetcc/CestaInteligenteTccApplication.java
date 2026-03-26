9a27054758580abf15cf092348bbb3ba6f494e9c
Marcos Dias <marcosaurelioce@hotmail.com>
2026-03-26 09:00:19 -0300

    commit inicial do projeto

---
+++ src/main/java/com/marcosdias/cestainteligentetcc/CestaInteligenteTccApplication.java
@@ -0,0 +1,13 @@
+package com.marcosdias.cestainteligentetcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CestaInteligenteTccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CestaInteligenteTccApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcosdias/cestainteligentetcc/CestaInteligenteTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosdias.cestainteligentetcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CestaInteligenteTccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
